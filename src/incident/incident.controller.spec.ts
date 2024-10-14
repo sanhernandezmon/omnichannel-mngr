93508d066aedc5394c179d15c4b614e7ed67ff93
sanhernandezmon <elsantimontano@gmail.com>
2024-10-13 20:31:57 -0500

    test passing tests :white-check-mark:

--- src/incident/incident.controller.spec.ts
+++ src/incident/incident.controller.spec.ts
@@ -1,11 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { IncidentController } from './incident.controller';
+import { SnsService } from '../sns/sns.service';
+
+const snsServiceMock = jest.fn().mockReturnValue({publishMessage: jest.fn()})
 
 describe('IncidentController', () => {
   let controller: IncidentController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [{provide: SnsService, useValue: snsServiceMock}],
       controllers: [IncidentController],
     }).compile();
 
--- src/sns/sns.service.spec.ts
+++ src/sns/sns.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SnsService } from './sns.service';
+import { ConfigService } from '@nestjs/config/dist/config.service';
 
 describe('SnsService', () => {
   let service: SnsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SnsService],
+      providers: [SnsService, ConfigService],
     }).compile();
 
     service = module.get<SnsService>(SnsService);
